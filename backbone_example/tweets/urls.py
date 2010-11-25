3bd7325f8e2f31ad5bfd4ad90bfef411f0c58148
Josh Bohde <josh.bohde@gmail.com>
2010-11-24 21:53:56 -0600

    Have a basic page being rendered by a view.

--- backbone_example/tweets/urls.py
+++ backbone_example/tweets/urls.py
@@ -4,6 +4,9 @@ from tweets.api import TweetResource
 tweet_resource = TweetResource()
 
 urlpatterns = patterns('',
+    (r'^$',
+     'django.views.generic.simple.direct_to_template',
+     {'template':'index.html'}),
     (r'^api/', include(tweet_resource.urls)),
 )
 
--- backbone_example/urls.py
+++ backbone_example/urls.py
@@ -14,3 +14,11 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # (r'^admin/', include(admin.site.urls)),
 )
+
+
+from django.conf import settings
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve',
+         {'document_root': settings.MEDIA_ROOT}),
+    )
