4ad8dd6a2cf8c1e0fccd2251a647aa406d1b0d29
Josh Bohde <josh.bohde@gmail.com>
2010-11-25 17:09:52 -0600

    If the server sends CREATED back, let's grab the model.

--- backbone_example/media/js/app.js
+++ backbone_example/media/js/app.js
@@ -1,4 +1,21 @@
 $(function(){
+    var oldSync = Backbone.sync;
+
+    Backbone.sync = function(method, model, success, error){
+        var newSuccess = function(resp, status, xhr){
+            if(xhr.statusText === "CREATED"){
+                var location = xhr.getResponseHeader('Location');
+                return $.ajax({
+                           url: location,
+                           success: success
+                       });
+            }
+            return success(resp);
+        };
+        return oldSync(method, model, newSuccess, error);
+    };
+   
+
     window.Tweet = Backbone.Model.extend({
       url: function(){
          return this.get('resource_uri') || this.collection.url;
