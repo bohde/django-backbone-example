42564623dce3b29a51dc7471a8a1d12acf65146f
Josh Bohde <josh.bohde@gmail.com>
2012-01-29 22:19:30 -0600

    Comment the collection helper methods.

--- backbone_example/static/js/app.js
+++ backbone_example/static/js/app.js
@@ -8,10 +8,14 @@
         model: Tweet, 
 
         maybeFetch: function(options){
+            // Helper function to fetch only if this collection has not been fetched before.
             if(this._fetched){
+                // If this has already been fetched, call the success, if it exists
                 options.success && options.success();
                 return;
             }
+
+            // when the original success function completes mark this collection as fetched
             var self = this,
                 successWrapper = function(success){
                     return function(){
@@ -24,6 +28,7 @@
         },
 
         getOrFetch: function(id, options){
+            // Helper function to use this collection as a cache for models on the server
             var model = this.get(id);
 
             if(model){
